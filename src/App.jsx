53bb692864bbd789f461cf43695073bb6ff3e1b4
NamTaeHo <han127kr@gmail.com>
2024-05-17 16:13:29 +0900

    Todo 수정하기

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,30 @@ function App() {
     setTodos([newTodo, ...todos]);
   };
 
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에
+    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    //인수: todos 배열에서 targetId 와 일치하는 id를 갖는 요소릐 데이터만 딱 바꾼 새로운 배열을 전달
+
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === targetId) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} />
     </div>
   );
 }
